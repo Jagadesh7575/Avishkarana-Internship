77e7f172d92b02a47deaa0acae4d60688f61dc1d
NANII <jagadesh@Saanikommus-MacBook-Air.local>
2026-06-04 16:21:52 +0530

    Add Day 1-2 internship files

---
+++ Day 1-2/basic.js
@@ -0,0 +1,13 @@
+let name = "Jagadesh";
+let age = 19;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+function add(a,b){
+    console.log("sum", a+b);
+}
+
+add(5,10);
